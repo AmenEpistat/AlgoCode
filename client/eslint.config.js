5e788e2afb0b5c09cd12adb23ede858bd2860ae5
luiza <luizaxakieva@gmail.com>
2026-01-22 21:36:15 +0300

    chore: configure ESLint with TypeScript, React, Prettier, single quotes

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,23 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import ts from '@typescript-eslint/eslint-plugin';
+import react from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default [
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parser: tsParser,
+            parserOptions: { ecmaFeatures: { jsx: true } },
+        },
+        plugins: {
+            '@typescript-eslint': ts,
+            react,
+            'jsx-a11y': jsxA11y,
+            prettier,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...ts.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            ...jsxA11y.configs.recommended.rules,
+
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    jsxSingleQuote: true,
+                    semi: true,
+                    trailingComma: 'es5',
+                    bracketSpacing: true,
+                    arrowParens: 'always',
+                    printWidth: 80,
+                    tabWidth: 4,
+                },
+            ],
+
+            quotes: ['error', 'single'],
+            'react/self-closing-comp': 'error',
+            'react/react-in-jsx-scope': 'off',
+        },
+        settings: {
+            react: { version: 'detect' },
+        },
     },
-  },
-])
+];
