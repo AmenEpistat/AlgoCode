3b6c9c4dbf1712a1422f3328baf7db1c69ab52dd
luiza <luizaxakieva@gmail.com>
2026-01-25 13:34:15 +0300

    chore: fix stylelint configuration

--- client/stylelint.config.js
+++ client/stylelint.config.js
@@ -1,8 +1,5 @@
 export default {
-    extends: [
-        'stylelint-config-standard-scss',
-        'stylelint-config-prettier',
-    ],
+    extends: ['stylelint-config-standard-scss'],
     plugins: ['stylelint-scss'],
     rules: {
         'scss/at-import-no-partial-leading-underscore': null,
