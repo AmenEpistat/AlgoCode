791ed8195e79f118fdfddae3e6e1cd7936d8ff1d
luiza <luizaxakieva@gmail.com>
2026-01-26 21:59:24 +0300

    feat: add Plank component

---
+++ client/src/components/Plank/Plank.tsx
@@ -0,0 +1,23 @@
+import { classNames } from '../../utils/classNames.ts';
+import styles from './Plank.module.scss';
+
+interface TabletProps {
+    title: string;
+    progress: number;
+    isLocked: boolean;
+}
+
+const Plank = ({ title, progress, isLocked }: TabletProps) => {
+    return (
+        <div className={classNames(styles.plank, isLocked && styles.locked)}>
+            <div className={styles.plank__title}>{title}</div>
+            {isLocked ? (
+                <div className={styles.plank__lock} />
+            ) : (
+                <div className={styles.plank__progress}>{progress}</div>
+            )}
+        </div>
+    );
+};
+
+export default Plank;
