1523dd49d717cfa89f4d342f5d22be5c430850bb
luiza <luizaxakieva@gmail.com>
2026-01-31 21:15:47 +0300

    chore: add shared Text interface

--- client/src/types/module.ts
+++ client/src/types/module.ts
@@ -1,26 +1,23 @@
-export interface Module {
-    id: string;
-    title: string;
-    description: string;
+import type { Text } from '@/types/text.ts';
+
+export interface Module extends Text {
     difficulty: number;
     order: number;
-    progress: number | null;
+    progress: number;
 }
 
 export interface ModuleDetails extends Module {
     steps: Step[];
 }
 
-export interface Step {
-    id: string;
-    title: string;
-    description: string;
+export interface Step extends Text {
+    progress: number;
     tasks: Task[];
 }
 
-export interface Task {
-    id: string;
-    title: string;
-    description: string;
-    type: 'theory' | 'code' | 'test' | 'exam';
+export interface Task extends Text {
+    type: TaskType;
+    progress: number;
 }
+
+export type TaskType = 'theory' | 'code' | 'test' | 'exam';
