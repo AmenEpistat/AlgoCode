485d4dfcf9824ec05995e6164fbff44e67f77406
luiza <luizaxakieva@gmail.com>
2026-02-01 19:18:38 +0300

    chore: add clarifying entities

--- client/src/types/task.ts
+++ client/src/types/task.ts
@@ -6,3 +6,32 @@ export interface Task extends Text {
 }
 
 export type TaskType = 'theory' | 'code' | 'test';
+
+export interface TaskCodeType extends Task {
+    type: 'code';
+    language: 'ts' | 'js';
+    starterCode: string;
+    tests: CodeTest[];
+    point: number;
+}
+
+export interface CodeTest {
+    id: number;
+    description: string;
+    run: (userCode: string) => boolean;
+}
+
+export interface TestResult {
+    passed: number;
+    total: number;
+    score: number;
+    lastFailed: CodeTest | null;
+}
+
+export interface TaskTheory extends Task {
+    type: 'theory';
+}
+
+export interface TaskTest extends Task {
+    type: 'test';
+}
