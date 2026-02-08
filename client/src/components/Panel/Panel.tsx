b8ecd9c8ea585fc7bee70221e25249c93b7e4e11
luiza <luizaxakieva@gmail.com>
2026-02-08 19:41:15 +0300

    feat(ui): update Panel component to be stateless with isExpanded prop

--- client/src/components/Panel/Panel.tsx
+++ client/src/components/Panel/Panel.tsx
@@ -1,16 +1,30 @@
 import styles from './Panel.module.scss';
 import { classNames } from '@/utils/classNames.ts';
 import React, { type CSSProperties } from 'react';
+import { ExpandOutlined, CompressOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
 interface PanelProps {
     className?: string;
     style?: CSSProperties;
     children: React.ReactNode;
     title: string;
-    icon: any;
+    icon: React.ReactNode;
+    onExpandPanel: () => void;
+    onCollapsePanel: () => void;
+    isExpanded: boolean;
 }
 
-const Panel = ({ className, children, title, icon, style }: PanelProps) => {
+const Panel = ({
+    className,
+    children,
+    title,
+    icon,
+    style,
+    onExpandPanel,
+    onCollapsePanel,
+    isExpanded,
+}: PanelProps) => {
     return (
         <div
             className={classNames(
@@ -22,6 +36,23 @@ const Panel = ({ className, children, title, icon, style }: PanelProps) => {
             <div className={styles['panel__title']}>
                 {icon}
                 {title}
+                <div className={styles['panel__buttons']}>
+                    <Button
+                        icon={
+                            !isExpanded ? (
+                                <ExpandOutlined
+                                    className={styles['panel__icon']}
+                                />
+                            ) : (
+                                <CompressOutlined
+                                    className={styles['panel__icon']}
+                                />
+                            )
+                        }
+                        type='text'
+                        onClick={!isExpanded ? onExpandPanel : onCollapsePanel}
+                    />
+                </div>
             </div>
             {children}
         </div>
