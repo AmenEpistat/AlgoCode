0f8d45a1216b8101e2885f05c66669285e2743bd
luiza <luizaxakieva@gmail.com>
2026-02-08 19:41:46 +0300

    refactor(task-code-page): extract TaskDescription into a separate component

---
+++ client/src/components/Tasks/TaskDescription/TaskDescription.tsx
@@ -0,0 +1,43 @@
+import { CodeOutlined } from '@ant-design/icons';
+import DifficultyTag from '@/components/DifficultyTag/DifficultyTag.tsx';
+import Panel from '@/components/Panel/Panel.tsx';
+import type { TaskCodeType } from '@/types/task.ts';
+import styles from './TaskDescription.module.scss';
+
+interface Props {
+    task: TaskCodeType;
+    isExpanded: boolean;
+    onExpand: () => void;
+    onCollapse: () => void;
+}
+
+const TaskDescription = ({
+    task,
+    isExpanded,
+    onExpand,
+    onCollapse,
+}: Props) => {
+
+    return (
+        <Panel
+            className={styles['task-description']}
+            title='Description'
+            isExpanded={isExpanded}
+            icon={<CodeOutlined />}
+            onExpandPanel={onExpand}
+            onCollapsePanel={onCollapse}
+        >
+            <div className={styles['task-description__content']}>
+                <h2 className={styles['task-description__title']}>
+                    {task.title}
+                </h2>
+                <DifficultyTag difficulty={task.difficulty} />
+                <p className={styles['task-description__text']}>
+                    {task.description}
+                </p>
+            </div>
+        </Panel>
+    );
+};
+
+export default TaskDescription;
