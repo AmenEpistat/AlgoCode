48ba32a0f08028a34b99904e006849d7f94776d9
luiza <luizaxakieva@gmail.com>
2026-02-15 14:02:30 +0300

    feat(task-code): add autocomplete toggle button to TaskCodeControls

--- client/src/components/Tasks/TaskCode/TaskCodeControls.tsx
+++ client/src/components/Tasks/TaskCode/TaskCodeControls.tsx
@@ -1,6 +1,11 @@
 import styles from './TaskCode.module.scss';
 import { Button, Select } from 'antd';
-import { AlignLeftOutlined, UndoOutlined } from '@ant-design/icons';
+import {
+    AlignLeftOutlined,
+    UndoOutlined,
+    LockFilled,
+    UnlockOutlined,
+} from '@ant-design/icons';
 import type { CodeLanguage } from '@/types/code.ts';
 
 interface Props {
@@ -8,6 +13,8 @@ interface Props {
     starterCode: Record<CodeLanguage, string>;
     onFormat: () => void;
     onReset: () => void;
+    onSwitchAuto: () => void;
+    isAutoSuggest: boolean;
     setLanguage: (language: CodeLanguage) => void;
 }
 
@@ -17,6 +24,8 @@ const TaskCodeControls = ({
     starterCode,
     onFormat,
     onReset,
+    onSwitchAuto,
+    isAutoSuggest,
 }: Props) => {
     return (
         <div className={styles['task-code-wrapper']}>
@@ -31,6 +40,14 @@ const TaskCodeControls = ({
                 }))}
             />
             <div className={styles['task-code__controls']}>
+                <Button
+                    className={styles['task-code__control-lock']}
+                    icon={isAutoSuggest ? <UnlockOutlined /> : <LockFilled />}
+                    type='text'
+                    onClick={onSwitchAuto}
+                >
+                    Auto
+                </Button>
                 <Button
                     icon={<AlignLeftOutlined />}
                     type='text'
