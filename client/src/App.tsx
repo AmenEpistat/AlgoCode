a99d06a4303187185a3c172bd6421c972cae7d71
luiza <luizaxakieva@gmail.com>
2026-02-28 20:21:33 +0300

    feat(ui): move AchievementModal to App root for global access

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.ts';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '@/store/StoreContext';
+import { AchievementModal } from '@/components/Achievement/AchievementModal/AchievementModal';
 
-function App() {
-    return <RouterProvider router={router} />;
-}
+const App = observer(() => {
+    const { userStore } = useStore();
+
+    return (
+        <>
+            <RouterProvider router={router} />
+
+            <AchievementModal
+                isOpen={userStore.isModalOpen}
+                onClose={() => userStore.closeModal()}
+                achievement={userStore.activeAchievement}
+            />
+        </>
+    );
+});
+
 export default App;
