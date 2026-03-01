e249aca240f22dbc622f13f4ad4a3ce9f5cf463c
luiza <luizaxakieva@gmail.com>
2026-03-01 21:13:25 +0300

    feat: implement yearly activity generator in userstore

--- client/src/store/UserStore.ts
+++ client/src/store/UserStore.ts
@@ -1,6 +1,9 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, reaction } from 'mobx';
 import type { Achievement } from '@/types/achievement';
 import type { RootStore } from './RootStore';
+import type { ActivityEvent } from '@/types/activity.ts';
+
+const STORAGE_KEY = 'user_data';
 
 export class UserStore {
     root: RootStore;
@@ -8,6 +11,7 @@ export class UserStore {
     username: string = 'Amen';
     xp: number = 0;
     streak: number = 0;
+    activity: Record<string, ActivityEvent[]> = {};
 
     unlockedAchievementIds: Set<string> = new Set();
     completedTaskIds: Set<string> = new Set();
@@ -18,6 +22,120 @@ export class UserStore {
     constructor(root: RootStore) {
         this.root = root;
         makeAutoObservable(this, { root: false });
+
+        this.loadFromStorage();
+
+        reaction(
+            () => ({
+                xp: this.xp,
+                streak: this.streak,
+                activity: this.activity,
+                unlockedAchievementIds: Array.from(this.unlockedAchievementIds),
+                completedTaskIds: Array.from(this.completedTaskIds),
+            }),
+            (data) => {
+                localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+            }
+        );
+    }
+
+    private loadFromStorage() {
+        const saved = localStorage.getItem(STORAGE_KEY);
+        if (saved) {
+            try {
+                const data = JSON.parse(saved);
+                this.xp = data.xp || 0;
+                this.streak = data.streak || 0;
+                this.activity = data.activity || {};
+                this.unlockedAchievementIds = new Set(
+                    data.unlockedAchievementIds || []
+                );
+                this.completedTaskIds = new Set(data.completedTaskIds || []);
+            } catch (e) {
+                console.error('Ошибка загрузки данных', e);
+            }
+        } else {
+            this.activity = {
+                '2026-01-12': [
+                    {
+                        id: '1',
+                        type: 'code',
+                        title: 'JS Basics',
+                        xp: 100,
+                        timestamp: '...',
+                    },
+                ],
+                '2026-02-01': [
+                    {
+                        id: '2',
+                        type: 'quiz',
+                        title: 'React Quiz',
+                        xp: 50,
+                        timestamp: '...',
+                    },
+                ],
+                '2026-02-14': [
+                    {
+                        id: '3',
+                        type: 'theory',
+                        title: 'Flexbox',
+                        xp: 120,
+                        timestamp: '...',
+                    },
+                ],
+                '2026-02-28': [
+                    {
+                        id: '4',
+                        type: 'code',
+                        title: 'Algorithms',
+                        xp: 300,
+                        timestamp: '...',
+                    },
+                ],
+                '2026-03-01': [
+                    {
+                        id: '5',
+                        type: 'code',
+                        title: 'MobX Intro',
+                        xp: 80,
+                        timestamp: '...',
+                    },
+                ],
+            };
+        }
+    }
+
+    get calendarData() {
+        const data = [];
+        const today = new Date();
+
+        for (let i = 0; i <= 364; i++) {
+            const date = new Date();
+            date.setDate(today.getDate() - i);
+            const dateStr = date.toISOString().split('T')[0];
+
+            const dayEvents = this.activity[dateStr] || [];
+            const totalDayXP = dayEvents.reduce(
+                (sum, event) => sum + event.xp,
+                0
+            );
+
+            let level = 0;
+            if (totalDayXP > 0 && totalDayXP < 50) level = 1;
+            else if (totalDayXP >= 50 && totalDayXP < 150) level = 2;
+            else if (totalDayXP >= 150 && totalDayXP < 300) level = 3;
+            else if (totalDayXP >= 300) level = 4;
+
+            data.push({
+                date: dateStr,
+                count: dayEvents.length,
+                level: level,
+            });
+        }
+
+        return data.sort(
+            (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+        );
     }
 
     get level() {
