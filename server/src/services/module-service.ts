a89bb0ce99086c30fe7630eb732d57df7a4cac08
luiza <luizaxakieva@gmail.com>
2026-03-13 23:13:36 +0300

    refactor: edit mongoose methods

--- server/src/services/module-service.ts
+++ server/src/services/module-service.ts
@@ -4,16 +4,15 @@ import ApiError from '../exceptions/api-error';
 
 export class ModuleService {
     async getModuleWithSteps(slug: string | string[]) {
-        const moduleDoc = await Module.findOne({ slug });
+        const moduleDoc = await Module.findOne({ slug }).lean();
 
         if (!moduleDoc) {
             throw ApiError.NotFound('Модуль не найден');
         }
 
-        const tasks = await Task.find({ moduleId: moduleDoc._id }).sort({
-            stepNumber: 1,
-            order: 1,
-        });
+        const tasks = await Task.find({ moduleId: moduleDoc._id })
+            .sort({ stepNumber: 1, order: 1 })
+            .lean();
 
         const stepsMap = tasks.reduce((acc: any[], task: any) => {
             const stepIdx = task.stepNumber - 1;
@@ -28,7 +27,7 @@ export class ModuleService {
             }
 
             acc[stepIdx].tasks.push({
-                ...task.toObject(),
+                ...task,
                 id: task._id.toString(),
             });
 
@@ -36,7 +35,7 @@ export class ModuleService {
         }, []);
 
         return {
-            ...moduleDoc.toObject(),
+            ...moduleDoc,
             id: moduleDoc._id.toString(),
             steps: stepsMap.filter(Boolean),
         };
