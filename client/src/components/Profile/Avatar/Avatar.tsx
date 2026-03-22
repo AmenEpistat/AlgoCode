5b663847919c35e48d3d15fd54219117fef14ceb
luiza <luizaxakieva@gmail.com>
2026-03-22 14:17:16 +0300

    style: edit styles for avatar

--- client/src/components/Profile/Avatar/Avatar.tsx
+++ client/src/components/Profile/Avatar/Avatar.tsx
@@ -1,9 +1,10 @@
 import { getAvatarUrl } from '@/utils/avatar.ts';
+import styles from './Avatar.module.scss';
 
 const Avatar = ({ avatarId }: { avatarId: string }) => {
     return (
-        <div>
-            <img src={getAvatarUrl(avatarId)} alt='avatar.' width='100px' />;
+        <div className={styles.avatar}>
+            <img src={getAvatarUrl(avatarId)} alt='avatar.' width='200px' />
         </div>
     );
 };
