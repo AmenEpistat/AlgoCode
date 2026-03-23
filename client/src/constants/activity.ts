3868de8b2c17768cd15155aa06028aeae20426c8
luiza <luizaxakieva@gmail.com>
2026-03-23 23:58:01 +0300

    chore: add constant

---
+++ client/src/constants/activity.ts
@@ -0,0 +1,9 @@
+export const XP_PER_LEVEL = 1000;
+
+export const ACTIVITY_LEVELS = [
+    { min: 0, level: 0 },
+    { min: 1, level: 1 },
+    { min: 50, level: 2 },
+    { min: 150, level: 3 },
+    { min: 300, level: 4 },
+] as const;
